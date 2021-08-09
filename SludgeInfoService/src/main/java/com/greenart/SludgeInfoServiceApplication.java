69cc441783c0dd699be62e466e9b322a1d556ba4
yujoonhan <sick1925@gmail.com>
2021-08-09 14:40:40 +0900

    added base codes

---
+++ SludgeInfoService/src/main/java/com/greenart/SludgeInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.greenart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SludgeInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SludgeInfoServiceApplication.class, args);
+	}
+
+}
---
+++ SludgeInfoService/src/test/java/com/greenart/SludgeInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SludgeInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
